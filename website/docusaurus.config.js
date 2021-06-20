d4ae3892cb2955a081034916de0411a0746d2827
Huynh Duc Dung <dung@aircarbon.co>
2021-06-20 14:43:11 +0800

    fix: github link on footer

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -53,7 +53,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/productsway/docusaurus',
+              href: 'https://github.com/jellydn/new-web-app',
             },
           ],
         },
@@ -70,13 +70,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/productsway/docusaurus/edit/master/website/',
+            'https://github.com/jellydn/new-web-app/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/productsway/docusaurus/edit/master/website/blog/',
+            'https://github.com/jellydn/new-web-app/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
