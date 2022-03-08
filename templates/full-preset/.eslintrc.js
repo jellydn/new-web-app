1cfebe681234221d6e618be7635d3e2bc09b931a
Huynh Duc Dung <dunghd.it@gmail.com>
2022-03-08 22:33:17 +0800

    chore(template): disable some unused rules for eslint

--- templates/full-preset/.eslintrc.js
+++ templates/full-preset/.eslintrc.js
@@ -3,4 +3,9 @@ module.exports = {
   parserOptions: {
     project: "./tsconfig.json",
   },
+  rules: {
+    "react/jsx-filename-extension": 0,
+    "import/extensions": 0,
+    "import/no-extraneous-dependencies": 0,
+  },
 };
