baf7ca989cfc1f47bce066013d2c4c21a7b82a3e
Huynh Duc Dung <dunghd.it@gmail.com>
2022-03-13 04:18:59 +0800

    chore: remove dist folder

--- dist/utils/tailwind.d.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "@oclif/core";
-declare class TailwindApp extends Command {
-    static description: string;
-    static flags: {
-        name: import("@oclif/core/lib/interfaces").OptionFlag<string | undefined>;
-    };
-    run(): Promise<void>;
-}
-export = TailwindApp;
