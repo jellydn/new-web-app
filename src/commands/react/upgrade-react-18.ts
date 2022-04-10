4e3d8c2f3daf1189d64b46c5a0981fcaeedfab6b
Huynh Duc Dung <dunghd.it@gmail.com>
2022-04-10 16:03:12 +0800

    feat(cli): add upgrade react 18 command

---
+++ src/commands/react/upgrade-react-18.ts
@@ -0,0 +1,32 @@
+import { Command } from "@oclif/core";
+import { CliUx } from "@oclif/core";
+import { exec } from "shelljs";
+
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
+class UpgradeReact18App extends Command {
+  static description = "Upgrade to React 18";
+
+  async run(): Promise<void> {
+    CliUx.ux.action.start(UpgradeReact18App.description);
+    exec(`yarn add react react-dom`);
+    exec(`yarn add -D @types/react @types/react-dom`);
+    exec(`npx types-react-codemod preset-18 .`);
+    this
+      .log(`React 18 introduces a new root API which provides better ergonomics for managing roots. The new root API also enables the new concurrent renderer, which allows you to opt-into concurrent features.
+    // Before
+    import { render } from 'react-dom';
+    const container = document.getElementById('app');
+    render(<App tab="home" />, container);
+
+    // After
+    import { createRoot } from 'react-dom/client';
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(<App tab="home" />);
+
+Happy hacking!`);
+    CliUx.ux.action.stop();
+  }
+}
+
+export = UpgradeReact18App;
